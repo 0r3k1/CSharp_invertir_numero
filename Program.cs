c7f161a2babbe72348e0c929f170a5845488adf5
Oreki <45185337+0r3k1@users.noreply.github.com>
2024-02-21 02:03:08 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numero_alreves {
+    class Program {
+        static void Main(string[] args) {
+            int num, count;
+            string linea, aux = "";
+
+            Console.Write("Ingrese un numero: ");
+            linea = Console.ReadLine();
+
+            count = linea.Length;
+
+            for (int i = 0; i < count; i++) {
+                num = int.Parse(linea) / int.Parse(Math.Pow(10, linea.Length - 1).ToString());
+                linea = (int.Parse(linea) % int.Parse(Math.Pow(10, linea.Length - 1).ToString())).ToString();
+                aux = num + aux;
+            }
+
+            Console.WriteLine(aux);
+        }
+    }
+}
